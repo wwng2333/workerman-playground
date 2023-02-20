3ee141f6a395b0457983e755463cc9a1335e18f2
test <wwng@2333.me>
2023-02-20 16:43:10 +0800

    Create main.php

---
+++ main.php
@@ -0,0 +1,64 @@
+<?php
+use Workerman\Worker;
+use Workerman\Connection\TcpConnection;
+use Workerman\Protocols\Http\Request;
+use Workerman\Protocols\Http\Response;
+use SebastianBergmann\Timer\Timer;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$http_worker = new Worker("http://0.0.0.0:2334");
+$http_worker->count = 2;
+$http_worker->name = 'jsdelivr';
+$http_worker->onMessage = function (TcpConnection $connection, Request $request) {
+    $timer = new Timer;
+    $timer->start();
+    $is_cached = 'missedCache';
+    if ($request->path() == '/npm' and $request->get('family')) {
+        $cache_file = '/tmp/jsdelivr_' . md5($request->uri());
+        if (is_file($cache_file)) {
+            $res = file_get_contents($cache_file);
+            $is_cached = 'cache; desc="Cache Read"';
+            echo $request->uri() . " cache hit\n";
+        } else {
+            echo $request->uri() . " new query\n";
+            $list = stristr($request->get('family'), '|') ? explode('|', $request->get('family')) : [$request->get('family')];
+            $res = '';
+            foreach ($list as $addr) {
+                $res .= curl('https://fastly.jsdelivr.net/npm/' . $addr);
+            }
+            file_put_contents($cache_file, $res);
+        }
+        $response = new Response(200, [
+            'Connection' => 'close',
+            'Cache-control' => 'max-age=86400'
+        ], $res);
+        $response->header('Content-Type', 'text/javascript;charset=UTF-8');
+    } else {
+        echo $request->uri() . " err\n";
+        $response = new Response(403, ['Connection' => 'close'], '<html>
+        <head><title>403 Forbidden</title></head>
+        <body>
+        <center><h1>403 Forbidden</h1></center>
+        <hr><center>workerman</center>
+        </body>
+        </html>');
+    }
+    $duration = $timer->stop();
+    $response->header('Server-Timing', $is_cached . '; dur=' . $duration);
+    $connection->close($response);
+};
+
+Worker::runAll();
+
+function curl($url)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
