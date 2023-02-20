4068064cf17476203baa5c8ce618ca606e815835
test <wwng@2333.me>
2023-02-20 20:08:00 +0800

    add /gh support

--- main.php
+++ main.php
@@ -14,7 +14,7 @@ $http_worker->onMessage = function (TcpConnection $connection, Request $request)
     $timer = new Timer;
     $timer->start();
     $is_cached = 'missedCache';
-    if ($request->path() == '/npm' and $request->get('family')) {
+    if (($request->path() == '/npm' or $request->path() == '/gh') and $request->get('family')) {
         $cache_file = '/tmp/jsdelivr_' . md5($request->uri());
         if (is_file($cache_file)) {
             $res = file_get_contents($cache_file);
@@ -25,7 +25,8 @@ $http_worker->onMessage = function (TcpConnection $connection, Request $request)
             $list = stristr($request->get('family'), '|') ? explode('|', $request->get('family')) : [$request->get('family')];
             $res = '';
             foreach ($list as $addr) {
-                $res .= curl('https://fastly.jsdelivr.net/npm/' . $addr);
+                $url = sprintf('https://fastly.jsdelivr.net%s/%s', $request->path(), $addr);
+                $res .= curl($url) . "\n";
             }
             file_put_contents($cache_file, $res);
         }
@@ -33,18 +34,11 @@ $http_worker->onMessage = function (TcpConnection $connection, Request $request)
             'Connection' => 'close',
             'Cache-control' => 'max-age=86400',
             'Content-Encoding' => 'gzip'
-            
         ], gzencode($res));
         $response->header('Content-Type', 'text/javascript;charset=UTF-8');
     } else {
         echo $request->uri() . " err\n";
-        $response = new Response(403, ['Connection' => 'close'], '<html>
-        <head><title>403 Forbidden</title></head>
-        <body>
-        <center><h1>403 Forbidden</h1></center>
-        <hr><center>workerman</center>
-        </body>
-        </html>');
+        $response = new Response(403, ['Connection' => 'close'], '<html><head><title>403 Forbidden</title></head><body><center><h1>403 Forbidden</h1></center><hr><center>workerman</center></body></html>');
     }
     $duration = $timer->stop();
     $response->header('X-Powered-By', 'https://github.com/wwng2333/jsdelivr-splice');
