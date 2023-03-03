5982af41841fb77be0d0ab826ba466f6dbdde712
test <wwng@2333.me>
2023-03-03 20:27:54 +0800

    Update myIP.php

--- myIP.php
+++ myIP.php
@@ -5,10 +5,22 @@ use Workerman\Connection\TcpConnection;
 use Workerman\Protocols\Http\Request;
 use Workerman\Protocols\Http\Response;
 
-$WhatIsYourIP = new Worker("http://[::]:2335");
-$WhatIsYourIP->count = 1;
-$WhatIsYourIP->name = 'WhatIsYourIP';
-$WhatIsYourIP->onMessage = function (TcpConnection $connection, Request $request) {
+$myip_v6 = new Worker("http://[::]:2331");
+$myip_v4 = new Worker("http://0.0.0.0:2332");
+$myip_v6->name = 'WhatIsYourIP';
+$myip_v4->name = 'WhatIsYourIP';
+$myip_v6->onMessage = function (TcpConnection $connection, Request $request) {
+    $ip = ($request->header('X-Real-IP')) ? 
+        $request->header('X-Real-IP') : $connection->getRemoteIp();
+    $response = new Response(200, [
+        'X-Powered-By' => 'Workerman ' . Worker::VERSION,
+        'Connection' => 'close',
+        'Content-Type' => 'text/plain; charset=UTF-8'
+    ], $ip);
+    $connection->close($response);
+};
+
+$myip_v4->onMessage = function (TcpConnection $connection, Request $request) {
     $ip = ($request->header('X-Real-IP')) ? 
         $request->header('X-Real-IP') : $connection->getRemoteIp();
     $response = new Response(200, [
