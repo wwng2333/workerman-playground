63f622cbd22fb0a3e2a99571aa21c570d4fe2871
test <wwng@2333.me>
2023-03-03 20:43:14 +0800

    Update myIP.php

--- myIP.php
+++ myIP.php
@@ -5,24 +5,25 @@ use Workerman\Connection\TcpConnection;
 use Workerman\Protocols\Http\Request;
 use Workerman\Protocols\Http\Response;
 
-$myip_v6 = new Worker("http://[::]:2331");
-$myip_v4 = new Worker("http://0.0.0.0:2332");
-$myip_v6->name = 'WhatIsYourIP';
-$myip_v4->name = 'WhatIsYourIP';
-$myip_v6->onMessage = function (TcpConnection $connection, Request $request) {
-    $ip = ($request->header('X-Real-IP')) ? 
-        $request->header('X-Real-IP') : $connection->getRemoteIp();
-    $response = new Response(200, [
-        'X-Powered-By' => 'Workerman ' . Worker::VERSION,
-        'Connection' => 'close',
-        'Content-Type' => 'text/plain; charset=UTF-8'
-    ], $ip);
-    $connection->close($response);
-};
+$context = array(
+    'ssl' => array(
+        'local_cert' => '/path_to/your.cer',
+        'local_pk' => '/path_to/your.key',
+        'verify_peer' => false,
+        'allow_self_signed' => false,
+    )
+);
 
-$myip_v4->onMessage = function (TcpConnection $connection, Request $request) {
+$myip = new Worker("http://[::]:443", $context);
+$myip->transport = 'ssl';
+$myip->name = 'WhatIsYourIP';
+$myip->onMessage = function (TcpConnection $connection, Request $request) {
     $ip = ($request->header('X-Real-IP')) ? 
         $request->header('X-Real-IP') : $connection->getRemoteIp();
+    if (!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+        list(, $ip) = explode('ffff:', $ip);
+        $ip = rtrim($ip, ']');
+    }
     $response = new Response(200, [
         'X-Powered-By' => 'Workerman ' . Worker::VERSION,
         'Connection' => 'close',
