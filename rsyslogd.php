ba640037dabee6e66156bb45eb5872664c3f6aa3
test <wwng@2333.me>
2023-03-04 15:34:39 +0800

    Update rsyslogd.php

--- rsyslogd.php
+++ rsyslogd.php
@@ -5,8 +5,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 $udp_worker = new Worker('udp://0.0.0.0:514');
 $udp_worker->name = 'rsyslogd';
 $udp_worker->onMessage = function ($connection, $data) {
-    list(, $data) = explode('>', $data);
-    $file = sprintf('/var/log/PSG1218/%s.log', $connection->getRemoteIp());
-    file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
+    if (strlen($data > 0)) {
+        list(, $data) = explode('>', $data);
+        $file = sprintf('/var/log/PSG1218/%s.log', $connection->getRemoteIp());
+        file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
+    }
 };
 Worker::runAll();
