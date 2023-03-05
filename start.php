8bcc324af9f20cbfc4a1341e4c204ba5904ee1de
test <wwng@2333.me>
2023-03-05 20:05:15 +0800

    Create start.php

---
+++ start.php
@@ -0,0 +1,12 @@
+<?php
+
+use Workerman\Worker;
+define('GLOBAL_START', true);
+require_once __DIR__ . '/vendor/autoload.php';
+#require_once __DIR__ . '/generate_204.php';
+require_once __DIR__ . '/jsdelivr.php';
+require_once __DIR__ . '/time.php';
+#require_once __DIR__ . '/http-proxy.php';
+require_once __DIR__ . '/ip.php';
+
+Worker::runAll();
