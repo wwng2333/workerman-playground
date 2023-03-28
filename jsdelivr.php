cc93f55bf27a0fce56f6940984f9cc359e6037b1
test <wwng@2333.me>
2023-03-28 15:51:05 +0800

    Update jsdelivr.php

--- jsdelivr.php
+++ jsdelivr.php
@@ -16,54 +16,57 @@ $jsdelivr_worker->onWorkerStart = function (Worker $worker) {
     $global = new GlobalData\Client('127.0.0.1:2207');
 };
 $jsdelivr_worker->onMessage = function (TcpConnection $connection, Request $request) {
-    global $global;
-    if ($request->path() === '/myipv4addr') {
-        $ip = ($request->header('X-Real-IP')) ? 
-            $request->header('X-Real-IP') : $connection->getRemoteIp();
-        $text = sprintf('var ipv4addr= document.getElementById("ipv4addr"); ipv4addr.innerHTML=\'Your IP: %s\'', $ip);
-        $response = new Response(200, ['Content-Type' => 'text/javascript'], $text);
-        $connection->close($response);
-    }
-    if ($request->path() === '/generate_204') {
-        $generate_204_response = new Response(204, [
-            'X-Powered-by' => 'github.com/wwng2333/generate_204',
-            'Connection' => 'close'
-        ]);
-        $connection->close($generate_204_response);
-    }
     $timer = new Timer;
     $timer->start();
-    $is_cached = 'missedCache';
-    if (in_array($request->path(), UPSTREAM) and $request->get('family')) {
-        $key_name = md5($request->uri());
-        if ($res = $global->$key_name) {
-            $is_cached = 'cache; desc="Cache Read"';
-        } else {
-            $list = stristr($request->get('family'), '|') ? explode('|', $request->get('family')) : [$request->get('family')];
-            $res = '';
-            foreach ($list as $addr) {
-                $url = sprintf('https://fastly.jsdelivr.net%s/%s', $request->path(), $addr);
-                $response = Requests::get($url);
-                $res .= $response->body . "\n";
+    $response = new Response(200, [
+        'Content-Security-Policy' => "img-src 'none'",
+        'X-Powered-by' => 'workerman',
+        'Connection' => 'close'
+    ]);
+    global $global;
+    switch ($request->path()) {
+        case '/myipv4addr':
+            $ip = ($request->header('X-Real-IP')) ?
+                $request->header('X-Real-IP') : $connection->getRemoteIp();
+            $response->withHeaders(['Content-Type' => 'text/javascript']);
+            $response->withBody(sprintf('var ipv4addr= document.getElementById("ipv4addr"); ipv4addr.innerHTML=\'Your IP: %s\'', $ip));
+            break;
+        case '/generate_204':
+            $response->withStatus(204);
+            break;
+        default:
+            $is_cached = 'missedCache; ';
+            if (in_array($request->path(), UPSTREAM) and $request->get('family')) {
+                $key_name = md5($request->uri());
+                if ($res = $global->$key_name) {
+                    $is_cached = 'cache; desc="Cache Read"; ';
+                } else {
+                    $list = stristr($request->get('family'), '|') ? explode('|', $request->get('family')) : [$request->get('family')];
+                    $res = '';
+                    foreach ($list as $addr) {
+                        $temp = Requests::get(sprintf('https://fastly.jsdelivr.net%s/%s', $request->path(), $addr));
+                        $res .= $temp->body . "\n";
+                    }
+                    $global->$key_name = $res;
+                }
+                if (stristr($request->header('Accept-Encoding'), 'gzip')) {
+                    $res = gzencode($res);
+                    $response->header('Content-Encoding', 'gzip');
+                }
+                $response->withHeaders([
+                    'Cache-control' => 'max-age=86400',
+                    'Access-Control-Allow-Origin' => '*',
+                    'Content-Type' => 'text/plain; charset=UTF-8'
+                ]);
+                $response->withBody($res);
+            } else {
+                $response->withStatus(403);
+                $response->withBody('<html><head><title>403 Forbidden</title></head><body><center><h1>403 Forbidden</h1></center><hr><center>workerman</center></body></html>');
             }
-            $global->$key_name = $res;
-        }
-        if (stristr($request->header('Accept-Encoding'), 'gzip'))
-            $res = gzencode($res);
-        $response = new Response(200, [
-            'Connection' => 'close',
-            'Cache-control' => 'max-age=86400',
-            'Access-Control-Allow-Origin' => '*',
-            'X-Powered-by' => 'workerman',
-            'Content-Type' => 'text/plain; charset=UTF-8'
-        ], $res);
-        if (stristr($request->header('Accept-Encoding'), 'gzip'))
-            $response->header('Content-Encoding', 'gzip');
-    } else {
-        $response = new Response(403, ['Connection' => 'close'], '<html><head><title>403 Forbidden</title></head><body><center><h1>403 Forbidden</h1></center><hr><center>workerman</center></body></html>');
+            break;
     }
     $duration = $timer->stop();
-    $response->header('Server-Timing', $is_cached . '; dur=' . $duration->asMilliseconds());
+    $response->header('Server-Timing', $is_cached . 'dur=' . $duration->asMilliseconds());
     $connection->close($response);
 };
 
