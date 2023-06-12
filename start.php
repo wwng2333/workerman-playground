833f7da778cca9e5dc722c048a9a41bc08dfc32d
test <wwng@2333.me>
2023-06-12 16:00:25 +0800

    Update start.php

--- start.php
+++ start.php
@@ -3,6 +3,7 @@
 use Workerman\Worker;
 define('GLOBAL_START', true);
 require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/GlobalDataServer.php';
 #require_once __DIR__ . '/generate_204.php';
 require_once __DIR__ . '/jsdelivr.php';
 require_once __DIR__ . '/time.php';
