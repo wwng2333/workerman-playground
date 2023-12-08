349460d9c0d461b8b99d8198bf2fbaa5bbd5b369
Crazy <17147265+wwng2333@users.noreply.github.com>
2023-12-08 13:22:48 +0800

    Update PhoneBatt.php

--- PhoneBatt.php
+++ PhoneBatt.php
@@ -7,20 +7,22 @@ use Workerman\Protocols\Http\Response;
 require_once __DIR__ . '/vendor/autoload.php';
 
 $batt = 0;
+$last_time = 0;
 
 $http_worker = new Worker("http://0.0.0.0:2485");
 $http_worker->count = 1;
 $http_worker->onMessage = function (TcpConnection $connection, Request $request) {
-    global $batt;
+    global $batt, $last_time;
     #var_dump($request);
     if ($request->get('batt')) {
         echo $request->get('batt') . "\n";
         $batt = $request->get('batt');
+        $last_time = GetMicrotime();
         $connection->close("recv ok!");
     }
     switch ($request->path()) {
         case '/metrics':
-            $connection->close("crazy_phone_batt ".$batt);
+            $connection->close(sprintf("crazy_phone_batt %s %s", $batt, $last_time));
             break;
         default:
             $connection->close(new Response(404, [], '404 not found'));
@@ -28,3 +30,8 @@ $http_worker->onMessage = function (TcpConnection $connection, Request $request)
 };
 
 Worker::runAll();
+
+function GetMicrotime(): int
+{
+    return (int) (microtime(true) * 1000);
+}
